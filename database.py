f7d74fe372734883395dc92a2c711c730e84d149
IanGrind <iangrindelwald@gmail.com>
2025-09-11 18:54:19 +0530

    Update database.py

--- database.py
+++ database.py
@@ -21,13 +21,29 @@ class Database:
     async def add_user(self, id, name):
         await self.col.insert_one({'id': id, 'name': name, 'ban_status': {'is_banned': False, 'ban_reason': ""}})
 
+    async def delete_user(self, user_id: int):
+        await self.col.delete_one({'id': int(user_id)})
+
     async def is_user_exist(self, id):
         return bool(await self.col.find_one({'id': int(id)}))
+        
+    async def get_all_users(self):
+        return self.col.find({})
 
     async def get_ban_status(self, id):
         user = await self.col.find_one({'id': int(id)})
         return user.get('ban_status', {'is_banned': False, 'ban_reason': ''}) if user else {'is_banned': False, 'ban_reason': ''}
-        
+
+    async def ban_user(self, user_id, ban_reason=""):
+        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': {'is_banned': True, 'ban_reason': ban_reason}}})
+
+    async def remove_ban(self, id):
+        await self.col.update_one({'id': id}, {'$set': {'ban_status': {'is_banned': False, 'ban_reason': ''}}})
+
+    async def get_banned(self):
+        users = self.col.find({'ban_status.is_banned': True})
+        return [user['id'] async for user in users]
+
     async def get_configs(self, id):
         default = {
             'caption': None, 'duplicate': True, 'forward_tag': False, 'file_size': 0, 'size_limit': None,
@@ -88,6 +104,13 @@ class Database:
 
     async def get_worker_bots(self, user_id):
         return [w async for w in self.worker.find({'user_id': user_id})]
+    
+    async def get_main_worker(self, user_id):
+        # Assuming the first worker bot added is the main one for now
+        workers = await self.get_worker_bots(user_id)
+        if workers:
+            return workers[0]
+        return None
 
     async def is_worker_bot_exist(self, user_id, bot_id):
         return bool(await self.worker.find_one({'user_id': user_id, 'id': bot_id}))
@@ -103,3 +126,18 @@ class Database:
 
     async def get_manager_userbot(self, user_id):
         return await self.manager.find_one({'user_id': user_id})
+
+    async def total_users_bots_count(self):
+        users_count = await self.col.count_documents({})
+        bots_count = await self.bot.count_documents({})
+        return users_count, bots_count
+
+    async def total_channels(self):
+        return await self.chl.count_documents({})
+
+    async def reset_user_data(self, user_id):
+        await self.bot.delete_many({'user_id': user_id})
+        await self.chl.delete_many({'user_id': user_id})
+        await self.worker.delete_many({'user_id': user_id})
+        await self.manager.delete_many({'user_id': user_id})
+        await self.col.update_one({'id': user_id}, {'$set': {'configs': {}}})
