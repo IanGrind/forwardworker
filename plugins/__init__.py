75d9d816e21f8d8895fbdcedcc4f0d80ea4f87d8
IanGrind <iangrindelwald@gmail.com>
2025-09-12 15:41:17 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,1 +1,10 @@
+# iangrind/forwardworker/forwardworker-1ff680b8c32922eb74e103a193e108a8d299c7bc/plugins/__init__.py
 
+from aiohttp import web
+from .route import routes
+
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
