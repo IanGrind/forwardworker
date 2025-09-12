193cb94726cdf70a01b4d8aea0f36def92ba5b95
IanGrind <iangrindelwald@gmail.com>
2025-09-12 15:48:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# iangrind/forwardworker/forwardworker-1ff680b8c32922eb74e103a193e108a8d299c7bc/main.py
+
 from database import initialize_database
 
-# Initialize the database BEFORE importing other modules that depend on it.
+# Initialize the database BEFORE importing the Bot class.
+# This ensures that when plugins are loaded, 'db' is a valid object.
 initialize_database()
 
 from bot import Bot
